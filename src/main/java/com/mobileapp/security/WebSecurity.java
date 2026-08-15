ce406405f0b595b29699c618a60a4683082e75f6
Harshgaddam <harshvardhangaddam@gmail.com>
2026-08-15 12:29:24 +0530

    Updated WebSecurity.java

--- src/main/java/com/mobileapp/security/WebSecurity.java
+++ src/main/java/com/mobileapp/security/WebSecurity.java
@@ -2,26 +2,19 @@ package com.mobileapp.security;
 
 import com.mobileapp.service.UserService;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
-
-    private static final Logger logger = LoggerFactory.getLogger(WebSecurity.class);
 
     private final UserService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -32,32 +25,18 @@ public class WebSecurity {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        logger.debug("Configuring SecurityFilterChain");
-        
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET, "/users").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                        .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
-        
-        logger.debug("SecurityFilterChain configured successfully");
+                .authorizeHttpRequests((authz) -> authz.requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .anyRequest().authenticated());
+
         return http.build();
     }
 
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        logger.debug("Configuring AuthenticationProvider with DaoAuthenticationProvider");
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
-        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
-        logger.debug("AuthenticationProvider configured with BCryptPasswordEncoder");
-        return authProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        logger.debug("Configuring AuthenticationManager");
-        return authConfig.getAuthenticationManager();
-    }
 }

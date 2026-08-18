905d38aa19c81775ac3b480f235a1e61778bbabb
Harshgaddam <harshvardhangaddam@gmail.com>
2026-08-18 14:42:56 +0530

    Implemented jwt Bearer in Response Header

--- src/main/java/com/mobileapp/security/WebSecurity.java
+++ src/main/java/com/mobileapp/security/WebSecurity.java
@@ -4,6 +4,7 @@ import com.mobileapp.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,11 +32,15 @@ public class WebSecurity {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers(HttpMethod.GET, SecurityConstants.SIGN_UP_URL).permitAll()
                         .requestMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated())
+                .authenticationManager(authenticationManager)
+                .addFilter(new AuthenticationFilter(authenticationManager));
 
         return http.build();
     }
